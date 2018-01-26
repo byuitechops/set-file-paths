8ddf8e2f87a96e8600d13389f973f0b59e3c23e3
Zachary Williams <wil11063@byui.edu>
2018-01-26 11:04:43 -0700

    Adjust for new filepaths

--- setFilePaths.js
+++ setFilePaths.js
@@ -15,13 +15,13 @@ module.exports = (course, stepCallback) => {
                 stepCallback(err, course);
             } else {
                 if (files.includes(folderName)) {
-                    setDirectory(filepath, folderName + ' - Copy', cb);
+                    setDirectory(filepath, folderName + '-Copy', cb);
                 } else {
                     cb(`${filepath}\\${folderName}`);
                 }
             }
         });
-    }
+    };
 
     var setZipPath = (filepath, fileName, cb) => {
         fs.readdir(filepath, (err, files) => {
@@ -30,21 +30,21 @@ module.exports = (course, stepCallback) => {
                 stepCallback(err, course);
             } else {
                 if (files.includes(fileName)) {
-                    setZipPath(filepath, fileName.split('.zip')[0] + ' - Copy.zip', cb);
+                    setZipPath(filepath, fileName.split('.zip')[0] + '-Copy.zip', cb);
                 } else {
                     cb(`${filepath}\\${fileName}`);
                 }
             }
         });
-    }
+    };
 
     /* Set the filepath we will be unzipping to */
-    setDirectory(course.info.unzippedFilepath, course.info.fileName.split('.zip')[0], (newDir) => {
-        course.info.unzippedFilepath = newDir;
-        setDirectory(course.info.altUnzippedFilepath, course.info.fileName.split('.zip')[0], (newDir) => {
-            course.info.altUnzippedFilepath = newDir;
-            setZipPath(course.info.zippedFilepath, course.info.fileName, (newDir) => {
-                course.info.zippedFilepath = newDir;
+    setDirectory(course.info.unzippedPath, course.info.fileName.split('.zip')[0], (newDir) => {
+        course.info.unzippedPath = newDir;
+        setDirectory(course.info.processedPath, course.info.fileName.split('.zip')[0], (newDir) => {
+            course.info.processedPath = newDir;
+            setZipPath(course.info.uploadZipPath, course.info.fileName, (newDir) => {
+                course.info.uploadZipPath = newDir;
                 stepCallback(null, course);
             });
         });
