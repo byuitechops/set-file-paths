7a426a5c8dbca5fe0df6a043e74cc0355b348466
Danverde <gre12014@byui.edu>
2018-02-06 10:46:09 -0700

    sets courseName & courseCode

--- setFilePaths.js
+++ setFilePaths.js
@@ -45,6 +45,16 @@ module.exports = (course, stepCallback) => {
             course.info.processedPath = newDir;
             setZipPath(course.info.uploadZipPath, course.info.fileName, (newDir) => {
                 course.info.uploadZipPath = newDir;
+
+                /* set courseName & courseCode */
+                if ((/\d{3}\w?/i).test(course.info.fileName)) {
+                    course.newInfo('courseName', course.info.fileName.split(/\d{3}\w?/i)[0].trim());
+                    course.newInfo('courseCode', course.info.fileName.match(/\d{3}\w?/i)[0]);
+                } else {
+                    course.newInfo('courseName', course.info.fileName.split('.zip')[0]);
+                    course.newInfo('courseCode', course.info.fileName.split('.zip')[0]);
+                }
+
                 stepCallback(null, course);
             });
         });
